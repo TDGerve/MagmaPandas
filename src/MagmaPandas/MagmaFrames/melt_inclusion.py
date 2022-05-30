ef813ddde9ce8f4ff531ae305c7080c92dbbceb5
TDGerve <thomasvangerve@gmail.com>
2022-05-30 21:18:50 +0200

    update

---
+++ src/MagmaPandas/MagmaFrames/melt_inclusion.py
@@ -0,0 +1,42 @@
+from typing import List
+import pandas as pd
+from .magmaFrame_baseclass import MagmaFrame
+from ..parse.readers import _read_file
+from .geochemistry import fO2, Fe_redox
+from .thermometers.melt import melt_thermometers
+from .melt import melt
+
+
+def read_melt(
+    filepath: str,
+    *args,
+    index_col: List[str],
+    total_col: str = None,
+    keep_columns: List[str] = None,
+    **kwargs,
+) -> "melt":
+    """
+    Read olivine compositions in wt. % oxide from a .csv file
+
+    """
+
+    return _read_file(
+        filepath=filepath,
+        *args,
+        phase="melt",
+        index_col=index_col,
+        total_col=total_col,
+        keep_columns=keep_columns,
+        units="wt. %",
+        Type="oxide",
+        **kwargs,
+    )
+
+
+class melt(melt):
+    def Fe_loss_correction(self):
+        """
+        Docstrings
+
+        """
+        # Do some corrections
