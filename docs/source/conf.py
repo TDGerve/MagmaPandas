5fae707af41eadedcd50b3a389f2dc99d209d5f3
TDGerve <thomasvangerve@gmail.com>
2024-03-14 17:47:46 +0100

    updated RTD config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 import pathlib
 import sys
 
-sys.path.insert(0, (pathlib.Path(__file__).parents[2] / "src").resolve().as_posix())
+# sys.path.insert(0, (pathlib.Path(__file__).parents[2] / "src").resolve().as_posix())
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
