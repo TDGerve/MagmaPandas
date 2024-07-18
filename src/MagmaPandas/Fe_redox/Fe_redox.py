65137b1af435c694c8748f27d12fd3312fbc0820
TDGerve <thomasvangerve@gmail.com>
2024-07-18 14:58:15 +0200

    fixed bug with calculate_Fe3Fe2 args

--- src/MagmaPandas/Fe_redox/Fe_redox.py
+++ src/MagmaPandas/Fe_redox/Fe_redox.py
@@ -33,4 +33,6 @@ def calculate_Fe3Fe2(mol_fractions, T_K, P_bar, fO2=None, **kwargs):
     if fO2 is None:
         fO2 = f.calculate_fO2(T_K=T_K, P_bar=P_bar, **kwargs)
 
-    return Fe_model.calculate_Fe3Fe2(mol_fractions, T_K, fO2, P_bar)
+    return Fe_model.calculate_Fe3Fe2(
+        melt_mol_fractions=mol_fractions, T_K=T_K, P_bar=P_bar, fO2=fO2
+    )
