76de6527c53422f5f1d02bcd2ff7c1f785e312da
Thomas van Gerve <thomasvangerve@gmail.com>
2025-09-18 11:08:08 +0200

    fixed nan referenced before assignment bug

--- src/MagmaPandas/thermometers/validate_temperatures.py
+++ src/MagmaPandas/thermometers/validate_temperatures.py
@@ -8,9 +8,13 @@ def _check_temperature(T_K):
     Find negative or NaN temperatures
     """
     try:
-        if (neg := any(T_K < 0)) or (nan := any(np.isnan(T_K))):
-            str_arr = np.array(["negative", "NaN"])[np.array([neg, nan])]
+        negative = any(T_K < 0)
+        nan = any(np.isnan(T_K))
+        if negative or nan:
+            str_arr = np.array(["negative", "NaN"])[np.array([negative, nan])]
             w.warn(f"{', '.join(str_arr)} temperatures found!")
     except TypeError:
-        if (neg := (T_K < 0)) or (nan := (T_K != T_K)):
-            str_arr = np.array(["negative", "NaN"])[np.array([neg, nan])]
+        negative = T_K < 0
+        nan = T_K != T_K
+        if negative or nan:
+            str_arr = np.array(["negative", "NaN"])[np.array([negative, nan])]
