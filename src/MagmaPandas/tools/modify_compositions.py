6bac03d5dfe5a43187fec8903b21cb06e688040e
Thomas van Gerve <thomasvangerve@gmail.com>
2025-09-18 11:07:27 +0200

    updated moles per O

--- src/MagmaPandas/tools/modify_compositions.py
+++ src/MagmaPandas/tools/modify_compositions.py
@@ -20,13 +20,13 @@ def _remove_elements(composition, drop: List[str]):
 
     composition_new = composition.copy()
 
-    for e in drop:
+    for element in drop:
 
-        if e in composition_new.index:
-            composition_new = composition_new.drop(e)
+        if element in composition_new.index:
+            composition_new = composition_new.drop(element)
         try:
-            if e in composition_new.columns:
-                composition_new = composition_new.drop(columns=[e])
+            if element in composition_new.columns:
+                composition_new = composition_new.drop(columns=[element])
         except AttributeError:
             pass
 
@@ -35,37 +35,49 @@ def _remove_elements(composition, drop: List[str]):
     return composition_new
 
 
-def moles_per_oxygen(moles):
+def cation_moles_per_oxygen(moles):
+    """
+    Calculates cations moles per 1 mole total oxygen. This conversion is used by the thermobarometers and Kd model from :cite:t:`Sun2020a`.
+
+    In the original paper this is described as 'oxide molar contents per unit oxygen'. However, Sun explained in emails that they are actually calculated as below, which is closer to cation moles per 1 mole total oxygen.
+    """
 
     axis = [0, 1][isinstance(moles, pd.DataFrame)]
-
+    # get number of oxygen per oxide
     O_number = e.oxygen_numbers(moles.elements)
-
+    # remove non-oxides
     no_oxygen = O_number.index[O_number == 0]
     composition_new = _remove_elements(composition=moles, drop=no_oxygen)
-
     O_number = O_number.drop(no_oxygen)
-    cations = e.cation_names(composition_new.elements)
+    # get number of cations per oxide
     cation_number = e.cation_numbers(composition_new.elements)
+    cations_per_oxygen = cation_number.div(O_number)
 
-    cations_normalised = cation_number.div(O_number)
-
-    names_new = {
-        ox: f"{cat}{Fraction(num).limit_denominator()}O"
-        for cat, num, ox in zip(cations, cations_normalised, composition_new.elements)
-    }
-    names_new["total"] = "O_total"
-
+    # calculate oxide moles per 1 mole total oxygen
     composition_new = (
         composition_new[composition_new.elements]
         .mul(O_number, axis=axis)
         .normalise(to=1)
     )
+    # convert to cations moles per 1 mole total oxygen
+    composition_new = composition_new.mul(cations_per_oxygen, axis=axis)
+
+    cations_names = e.cation_names(composition_new.elements)
+    names_new = {
+        ox: f"{cat}{Fraction(num).limit_denominator()}O"
+        for cat, num, ox in zip(
+            cations_names, cations_per_oxygen, composition_new.elements
+        )
+    }
+    names_new["total"] = "O_total"
 
     return composition_new.rename(names_new, axis=axis)
 
 
-def _anhydrous_composition(composition):
+def _anhydrous_composition(composition, normalise=True):
+
+    if ("H2O" not in composition.index) & ("H2O" not in composition.columns):
+        return composition.copy()
 
     composition_H2O = composition.copy()
 
@@ -73,5 +85,8 @@ def _anhydrous_composition(composition):
         composition_H2O = composition_H2O.drop("H2O")
     except KeyError:
         composition_H2O = composition_H2O.drop(columns=["H2O"])
+
+    if not normalise:
+        return composition_H2O.recalculate()
 
     return composition_H2O.recalculate().normalise()
