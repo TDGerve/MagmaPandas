e48255ef561dbe95746543b98b136eb2ac2f423f
Thomas van Gerve <thomasvangerve@gmail.com>
2025-09-23 10:20:48 +0200

    updated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,7 +60,7 @@ nb_execution_mode = "off"
 
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {"navigation_depth": 6, "body_max_width": None}
-html_static_path = ["../_static"]
+html_static_path = ["./_static"]
 html_css_files = [
     "custom.css",
 ]
